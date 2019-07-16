c6f2a53756534169c9c04f60881f7b6a5eabe00e
Nick <tick_lin@aliyun.com>
2019-07-16 16:54:52 +0800

    chore: test null assetion (!) at class property.

--- tests/no-non-null-assertion/bad.ts
+++ tests/no-non-null-assertion/bad.ts
@@ -1,3 +1,11 @@
 export function foo(instance: MyClass | undefined) {
   instance!.doWork();
 }
+
+class MyClass {
+  foo!: number;
+
+  doWork() {
+    return;
+  }
+}
--- tests/no-non-null-assertion/good.ts
+++ tests/no-non-null-assertion/good.ts
@@ -7,3 +7,10 @@ export function bar(instance: MyClass | undefined) {
     instance.doWork(); // 这里并不需要使用 ! 断言，因为TypeScript能推断出这里面的instance一定不为undefined
   }
 }
+class MyClass {
+  foo!: number;
+
+  doWork() {
+    return;
+  }
+}
